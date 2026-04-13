6d07306aca1044fe29d4dfaafbcb284cf91d47c2
Suriya <suriya.bala.mari@gmail.com>
2026-04-13 12:15:36 +0530

    removed wallet configuration

--- Wallet/src/main/java/com/example/repository/WalletRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.domain.WalletEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WalletRepository extends JpaRepository<WalletEntity,Integer> {
-}
